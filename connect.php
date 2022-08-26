eef3790670da083071c3ce0ab9111234b7093775
Saivaikant <109860370+Saivaikant@users.noreply.github.com>
2022-08-27 00:58:43 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,27 @@
+<?php
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$address = $_POST['address'];
+	$phno = $_POST['phno'];
+  $waste = $_POST['waste'];
+  $wastetype = $_POST['wastetype'];
+  $pickup = $_POST['pickup'];
+  $postal = $_POST['postal'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','test');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, address, phno, waste, wastetype, pickup, postal) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssiissi", $firstName, $lastName, $gender, $email, $address, $phno, $waste, $wastetype, $pickup, $postal);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
